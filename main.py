d8428e70423842a4301ca78263976b635da6d9da
dammaladineshpaul21 <dammaladineshpaul21@gmail.com>
2022-07-15 17:36:31 +0530

    Updated new connection

---
+++ main.py
@@ -0,0 +1,13 @@
+import mysql.connector
+import pandas as pd
+try:
+    mydb = mysql.connector.connect(user='root', password='Munna#21021995',
+                                            host='127.0.0.1', database="poidata")
+    mycursor = mydb.cursor()
+    get_data = pd.read_sql("select * from poidetails", mydb)
+    df = pd.DataFrame(get_data)
+    df["name"].fillna("No College", inplace=True)
+    print(df)
+
+except Exception as e:
+    print(e)
---
+++ user_Data.sql
@@ -0,0 +1,5 @@
+show databases;
+use poidata;
+create table poidetails(name char(50));
+insert into poidetails (name) values ("komal");
+select * from poidetails;
