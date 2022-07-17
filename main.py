97c9a34f8e84e69ed2c40974b69e27ceef1b4fa6
dammaladineshpaul21 <dammaladineshpaul21@gmail.com>
2022-07-17 16:45:34 +0530

    Update new file with new set of code

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import mysql.connector
 import pandas as pd
 try:
-    mydb = mysql.connector.connect(user='root', password='Munna#21021995',
-                                            host='127.0.0.1', database="poidata")
-    mycursor = mydb.cursor()
-    get_data = pd.read_sql("select * from poidetails", mydb)
-    df = pd.DataFrame(get_data)
-    df["name"].fillna("No College", inplace=True)
-    print(df)
+    cnx = mysql.connector.connect(user="root", password="Munna#21021995", host='127.0.0.1', port=3306, database="poidata")
+    hold_db = cnx.cursor()
+    # hold_db.execute("Create database if not exists poidata;")
+    # # hold_db.execute("Show Databases;")
+    # hold_db.execute("use poidata;")
+    # hold_db.execute("Create table if not exists poi_details(poi_name char(50));")
+    getdata = hold_db.fetchall()
+    for i in getdata:
+        print(i)
+except Exception as err:
+    print(err)
 
-except Exception as e:
-    print(e)
+
+
+
--- user_Data.sql
+++
@@ -1,5 +0,0 @@
-show databases;
-use poidata;
-create table poidetails(name char(50));
-insert into poidetails (name) values ("komal");
-select * from poidetails;
