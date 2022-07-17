edda15a5f3364af648d34547fe3257f5eceff83a
dammaladineshpaul21 <dammaladineshpaul21@gmail.com>
2022-07-17 16:48:16 +0530

    New update's

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import pandas as pd
 try:
+<<<<<<< HEAD
     mydb = mysql.connector.connect(user='root', password='Munna#21021995',
                                             host='127.0.0.1', database="poidata")
     mycursor = mydb.cursor()
@@ -8,6 +9,20 @@ try:
     df = pd.DataFrame(get_data)
     df["name"].fillna("No College", inplace=True)
     print(mycursor)
+=======
+    cnx = mysql.connector.connect(user="root", password="Munna#21021995", host='127.0.0.1', port=3306, database="poidata")
+    hold_db = cnx.cursor()
+    # hold_db.execute("Create database if not exists poidata;")
+    # # hold_db.execute("Show Databases;")
+    # hold_db.execute("use poidata;")
+    # hold_db.execute("Create table if not exists poi_details(poi_name char(50));")
+    getdata = hold_db.fetchall()
+    for i in getdata:
+        print(i)
+except Exception as err:
+    print(err)
 
-except Exception as e:
-    print(e)
+
+
+>>>>>>> 97c9a34 (Update new file with new set of code)
+
